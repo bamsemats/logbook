e126dfb35f35405c3c7dadcdeecbc48ee2985910
bamsemats <mats.f.ronnqvist@gmail.com>
2025-05-08 10:07:56 +0200

    Initialise project

---
+++ src/components/logbook/logbook.jsx
@@ -0,0 +1,12 @@
+import './logbook.css';
+import {useState, useEffect} from 'react';
+
+export default function Logbook() {
+  return (
+    <div className='logbook-container'>
+      <h2>
+        This is a logbook thingy
+      </h2>
+    </div>
+  )
+}
