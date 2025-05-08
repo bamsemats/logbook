30c954efc037fe032f330747d5e83f75118dbeab
bamsemats <mats.f.ronnqvist@gmail.com>
2025-05-08 13:41:38 +0200

    Set up a few colors and get the hover effect on buttons to work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Logbook from './components/logbook/logbook';
 
 function App() {
   return (
     <div className="App">
-   
+      <Logbook />
     </div>
   );
 }
--- src/components/logbook/logbook.jsx
+++ src/components/logbook/logbook.jsx
@@ -1,12 +1,12 @@
+import Tiptap from '../tiptap/tiptap';
 import './logbook.css';
 import {useState, useEffect} from 'react';
 
 export default function Logbook() {
   return (
     <div className='logbook-container'>
-      <h2>
-        This is a logbook thingy
-      </h2>
+        <h2>This is an App!</h2>
+        <Tiptap />
     </div>
   )
 }
